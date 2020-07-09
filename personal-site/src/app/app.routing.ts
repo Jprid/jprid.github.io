d109d2cfd23c5435d8ffad7795c5ba5323d7a02a
Jprid <mackattack.bg@gmail.com>
2020-07-09 17:48:47 -0500

    Auto-generated commit

--- personal-site/src/app/app.routing.ts
+++
@@ -1,8 +0,0 @@
-import {Routes, RouterModule} from '@angular/router';
-import { PageComponent } from './page/page.component';
-const appRoutes = [
-    {path: ':key', component: PageComponent},
-    {path: '', redirectTo: 'About', pathMatch: 'full'}
-];
-
-export const Routing = RouterModule.forRoot(appRoutes);
