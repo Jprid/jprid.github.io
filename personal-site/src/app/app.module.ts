820e0decbc64e0198a9e7ebc62f894c604cf7e07
Jprid <mackattack.bg@gmail.com>
2020-11-17 20:28:34 -0600

    v3 less handheld device styling

--- personal-site/src/app/app.module.ts
+++ personal-site/src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { PageComponent } from './page/page.component';
 import { RouterModule } from '@angular/router';
 import { Routing } from './app.routing';
-import { PageElementComponent } from './page-element/page-element.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent,  PageComponent, PageElementComponent, AboutPageComponent
+    AppComponent, AboutPageComponent
   ],
   imports: [
     BrowserModule, RouterModule, Routing
--- personal-site/src/app/app.routing.ts
+++ personal-site/src/app/app.routing.ts
@@ -1,9 +1,7 @@
 import {Routes, RouterModule} from '@angular/router';
-import { PageComponent } from './page/page.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 const appRoutes = [
     {path: 'About', component: AboutPageComponent},
-    {path: ':key', component: PageComponent},
     {path: '', redirectTo: 'About', pathMatch: 'full'}
 ];
 
