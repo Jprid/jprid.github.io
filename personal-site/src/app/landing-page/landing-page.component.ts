fbb4bebabed4977ca047f2d001c99f9c1a663a6d
Jprid <mackattack.bg@gmail.com>
2021-02-03 19:05:50 -0600

    Update template for landing page component

--- personal-site/src/app/landing-page/landing-page.component.ts
+++ personal-site/src/app/landing-page/landing-page.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
-  template: `
-    <app-welcome-page></app-welcome-page>
+  template: `<app-welcome-page></app-welcome-page>
     <app-experience-list id="experience"></app-experience-list>
     <app-contact-page></app-contact-page>
   `,
